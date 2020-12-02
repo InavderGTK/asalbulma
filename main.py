fb2adf96e452a913b1fe66e01ec4d4f479db8251
Gökdeniz Tunç Tunç <gtkizilkaya72@gmail.com>
2020-12-02 13:04:10 +0000

    Gg

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-s = int(input("Başlangıç sayısı: "))
+s = float(input("Sayı girin: "))  
 f = 0
 i = 2
-
 while i <= s / 2:
-  if s % i == 0:
-    f = 1
-    add = False
-
-  i = i + 1
-  if f == 0:
-    print(str(s) + " asal sayıdır")
-    add = True
-  else:
-    add = True
-
-  while add:
-    s + 1
-    add = False
+    if s == 1:
+      print("error")
+      quit()
+    
+    if s % i == 0:
+        f=1
+        break
+    i=i+1
+    
+if f==0:
+    print("Asal Sayıdır")
+else:
+    print("Asal Sayı Değildir")
